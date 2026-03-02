ef4f5d08cec1e4248beafc4efa2e975616d50cac
Fedotos228 <tudor.onceanu@gmail.com>
2026-03-02 10:32:20 +0200

    cors settings

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,31 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from '@strapi/strapi'
 
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'https://savam.vercel.app/',
+        'savam-git-api-fedotos228s-projects.vercel.app',
+        process.env.SITE_URL_INT,
+        process.env.STRAPI_DOMAIN
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      keepHeaderOnError: true,
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]
 
-export default config;
+export default config
